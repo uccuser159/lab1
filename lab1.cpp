ec6e9d9741cd9c7fa00a6e7d51c3b4ec6d53702d
uccuser159 <uccuser159@gmail.com>
2019-04-23 19:11:00 +0800

    Question B

---
+++ lab1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int a;
+void fun();
+int main()
+
+{
+
+
+	cout << "Please insert a number:" ;
+	cin >> a;
+
+	cout << a<< " " ;
+
+	fun();
+
+	system("pause");
+
+	return 0;
+
+
+}
+
+void fun(){
+
+
+		if(a==1){
+		
+			
+
+		
+		}
+		else if(a%2==1){
+			a=3*a+1;
+			cout << a<< " ";
+			fun();
+		}
+		else{
+			a/=2;
+			cout << a<<" ";
+			fun();
+		}
+
+
+
+}
